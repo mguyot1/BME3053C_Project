822c2865632886c7a056ffe423c9a41f0e703b9b
mguyot1 <73506962+mguyot1@users.noreply.github.com>
2021-11-02 16:26:15 -0400

    Add files via upload

--- segment_lung.m
+++ segment_lung.m
@@ -6,17 +6,18 @@ for n = 1: total_num_images
     name = fullfile(image_folder, fileNames(n).name);
     names(n) = {fileNames(n).name}; 
     image = imread(name);
+    grayscale = im2gray(image);
+    threshold_value = 120;
+    binary_image = grayscale < threshold_value;
+    figure(n)
+    title(name)
+    subplot(1,3,1)
+    imshow(binary_image);
+     subplot(1,3,2)
+    lungs = bwareafilt(binary_image,2);
+    imshow(lungs)
+    props = regionprops(binary_image);
+    subplot(1,3,3)
+    imshow(image)
 end
-
-grayscale = rgb2gray(image);
-
-threshold_value = 120;
-
-binary_image = grayscale < threshold_value;
-
-imshow(binary_image);
-
-lungs = bwareafilt(binary_image,2);
-imshow(lungs)
-props = regionprops(binary_image)
 
