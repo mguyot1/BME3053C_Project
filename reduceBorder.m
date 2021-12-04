92e69d355f79a6e403cd5b755ed187c9bce5b830
cameronmanson <92829767+cameronmanson@users.noreply.github.com>
2021-12-04 02:00:26 -0500

    Add files via upload

---
+++ reduceBorder.m
@@ -0,0 +1,32 @@
+% Authors: Matthew Nguyen, Michael Guyot, Cameron Manson, Nathan Jones
+% Course: BME 3053C Computer Applications for BME
+% Term: Fall 2021
+% J. Crayton Pruitt Family Department of Biomedical Engineering
+% University of Florida
+
+%Removes a n pixel thick border from the segmented lungs
+function lungIm = reduceBorder(lungIm, n)
+lungIm1 = lungIm;
+w = length(lungIm(1,:));
+h = length(lungIm(:,1));
+for ii = 3:h-2
+    for jj = 3:w-2
+        if lungIm1((ii + n),(jj + n)) == 0
+            lungIm(ii,jj) = 0;
+        elseif lungIm1((ii - n),(jj + n)) == 0
+            lungIm(ii,jj) = 0;
+        elseif lungIm1((ii + n),(jj - n)) == 0
+            lungIm(ii,jj) = 0;
+        elseif lungIm1((ii - n),(jj - n)) == 0
+            lungIm(ii,jj) = 0;
+        elseif lungIm1((ii),(jj + n)) == 0
+            lungIm(ii,jj) = 0;
+        elseif lungIm1((ii),(jj - n)) == 0
+            lungIm(ii,jj) = 0;
+        elseif lungIm1((ii + n),(jj)) == 0
+            lungIm(ii,jj) = 0;
+        elseif lungIm1((ii - n),(jj)) == 0
+            lungIm(ii,jj) = 0;
+        end
+    end
+end
